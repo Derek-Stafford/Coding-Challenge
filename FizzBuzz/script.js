62dcac1cae30bdb60658c142583a6dcb0e3933fd
Boulder <157924017+Derek-Stafford@users.noreply.github.com>
2025-04-21 00:56:26 -0700

    Update script.js

--- FizzBuzz/script.js
+++ FizzBuzz/script.js
@@ -2,20 +2,20 @@ let number = [];
 let of_3 = [];
 let of_5 = [];
 let both = [];
-con1 = "Fizz"
-con2 = "Buzz"
-con3 = "FizzBuzz"
+condition_1 = "Fizz"
+condition_2 = "Buzz"
+condition_3 = "FizzBuzz"
 
 for (let i = 1; i <= 100; i++){
   if (i % 3 === 0 && i % 5 === 0) {
     both.push(i);
-    console.log(con3);
+    console.log(condition_3);
   } else if (i % 3 === 0) {
     of_3.push(i);
-    console.log(con1);
+    console.log(condition_1);
   } else if (i % 5 === 0) {
     of_5.push(i);
-    console.log(con2);
+    console.log(condition_2);
   } else {
     number.push(i);
   }
