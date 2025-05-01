16bfd7768086ea4b8e54c337ac312039c566ef07
Boulder <157924017+Derek-Stafford@users.noreply.github.com>
2025-05-01 15:26:16 -0700

    Update script.js

--- FizzBuzz/script.js
+++ FizzBuzz/script.js
@@ -1,26 +1,26 @@
 let number = [];
-let of_3 = [];
-let of_5 = [];
-let both = [];
+let divisible_by_3 = [];
+let divible_by_5 = [];
+let divisible_by_3and5 = [];
 condition_1 = "Fizz"
 condition_2 = "Buzz"
 condition_3 = "FizzBuzz"
 
 for (let i = 1; i <= 100; i++){
   if (i % 3 === 0 && i % 5 === 0) {
-    both.push(i);
+    divisible_by_3and5.push(i);
     console.log(condition_3);
   } else if (i % 3 === 0) {
-    of_3.push(i);
+    divisible_by_3.push(i);
     console.log(condition_1);
   } else if (i % 5 === 0) {
-    of_5.push(i);
+    divible_by_5.push(i);
     console.log(condition_2);
   } else {
     number.push(i);
   }
 }
-console.log(of_3);
-console.log(of_5);
-console.log(both);
-console.log(number);
+
+console.log("Numbers divisible by 3: ", divisible_by_3);
+console.log("Numbers divisible by 5: ", divisible_by_5);
+console.log("Numbers divisible by 3 and 5: ", divisible_by_3and5);
